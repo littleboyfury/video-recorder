3db859e9e77ba599b2265beb2b155c45bc8f3092
skyfury <littleboyfury@gmail.com>
2022-06-10 18:57:30 +0800

    init project

--- src/App.vue
+++ src/App.vue
@@ -178,7 +178,7 @@ export default {
     nextTab() {
       this.isSuccessTextShow = true
       setTimeout(() => {
-        // this.isSuccessTextShow = false
+        this.isSuccessTextShow = false
       }, 4000)
     }
   },
